b78222a0e792cff521b9f28bddbb553613a180b4
DotWho <dot.who@outlook.com>
2018-03-26 16:38:23 +0800

    update

---
+++ src/js/pages/test.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <text>Test</text>
+    </div>
+</template>
+
+<script>
+import { WxcButton } from 'weex-ui'
+export default {
+    components: { WxcButton },
+    data () {
+        return {
+
+        };
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style lang="sass">
+
+</style>
